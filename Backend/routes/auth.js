5f6dd7126ef221de715db0f17ad278f4f7e391a6
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-03-19 16:16:22 +0530

    user authentication added

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const User=require('../models/users')
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
 
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user= User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/',[
+    body('email',"Enter a Valid email").isEmail(),
+    body('password',"Password must be greater than 5 letters").isLength({ min: 5 }),
+    body('name',"Username must be valid").isLength({ min: 5 })
+
+], (req, res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        email:req.body.email,
+        password: req.body.password,
+      }).then(user => res.json(user));
 })
 
 module.exports = router
