c373ce43d916bf952ecbf377929d8972005a2ceb
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-03-28 18:01:43 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-
 import './App.css';
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Routes
+  
+} from "react-router-dom";
+
+import About from './Components/About';
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        
-      <h1>jkjkgu</h1>
-      </header>
+    <Navbar/>
+    <Routes>
+      <Route path="/" exact element={<Home/>} />
+      <Route path="/Navbar" exact element={<Navbar/>} />
+      <Route path="/About" exact element={<About/>} />
+      
+    </Routes>
     </div>
+    </>
   );
 }
 
